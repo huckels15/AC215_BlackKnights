8babeaf826ff6c21bf9c6db79f1c45e661985468
Elijah <elijahdabkowski@g.harvard.edu>
2024-10-18 11:05:23 -0400

    Added comments to data_download_resnet.py and docker-shell.sh

--- src/data_download_resnet/data_download_resnet.py
+++ src/data_download_resnet/data_download_resnet.py
@@ -1,52 +1,36 @@
 from google.cloud import storage
 import os
 
-# Set the environment variable for authentication
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secrets/secrets.json'
 
 def download_directory_from_gcs(bucket_name, source_directory, destination_directory):
     """
-    Downloads all files from a GCS directory to a local directory.
-
-    Args:
-        bucket_name (str): The name of the GCS bucket.
-        source_directory (str): The path to the directory in the GCS bucket (the "prefix").
-        destination_directory (str): The local directory where the files should be saved.
+    The workflow here is relatively straight forward. We make a client and then obtain the 
+    bucket that is specified within the function definition. We then proceed to obtain all of the 
+    blobs, or files, within the bucket. If the blobs name ends with a /, we know that it is a directory,
+    so in order to keep the same file structure we continue to sift through the blob until we reach
+    the actual file that is specified by the basename function. We then join that name with the 
+    userspecified destination and download that to the local machine. 
     """
-    # Create a Google Cloud Storage client
+
     storage_client = storage.Client()
-
-    # Get the bucket
     bucket = storage_client.bucket(bucket_name)
-
-    # Ensure the destination directory exists
-    if not os.path.exists(destination_directory):
-        os.makedirs(destination_directory)
-
-    # List all blobs in the GCS directory (with a given prefix)
     blobs = bucket.list_blobs(prefix=source_directory)
-
-    # Download each file in the directory
     for blob in blobs:
-        # Skip "directories" (GCS doesn't have real directories, just common prefixes)
         if blob.name.endswith("/"):
             continue
-
-        # Create local path for the downloaded file
-        file_name = os.path.basename(blob.name)  # Get the file name
+        
+        file_name = os.path.basename(blob.name)
         destination_file_name = os.path.join(destination_directory, file_name)
-
-        # Download the blob to the local file path
         blob.download_to_filename(destination_file_name)
 
-        print(f"Downloaded {blob.name} to {destination_file_name}")
+        print(f"Downloaded {blob.name}")
 
 
 if __name__ == '__main__':
     bucket_name = 'cancer-data-bucket' 
     source_directory = 'dvc_store/csvs/' 
     destination_directory = './data/' 
-
     download_directory_from_gcs(bucket_name, source_directory, destination_directory)
 
 
