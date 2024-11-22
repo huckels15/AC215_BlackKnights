0f53ecf730693fe92018cddf9984acdbaf52acb7
Elijah <elijahdabkowski@g.harvard.edu>
2024-11-21 19:45:05 -0500

    Image rendering

--- src/app-building/frontend/src/components/playground/DEEPFOOLAlex.jsx
+++ src/app-building/frontend/src/components/playground/DEEPFOOLAlex.jsx
@@ -1,9 +1,11 @@
 export const getDEEPFOOLAlex = async (model, attack) => {
     const url = "http://34.138.29.34:8000/alexnet-attack/";
+    const imageUrl =
+    "http://34.138.29.34:8000/get-file/?file_path=figures/example_1_original_vs_adversarial.png";
     const payload = {
       model: model,
       attack: attack,
-      max_iter: 10,
+      max_iter: 1,
     };
   
     try {
@@ -20,7 +22,20 @@ export const getDEEPFOOLAlex = async (model, attack) => {
       }
   
       const jsonResponse = await response.json();
-      return jsonResponse;
+
+      const imageResponse = await fetch(imageUrl);
+      if (!imageResponse.ok) {
+        throw new Error(`Failed to download image. Status: ${imageResponse.status}`);
+      }
+
+      const imageBlob = await imageResponse.blob();
+
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+
+      return {
+        json: jsonResponse,
+        image: imageObjectURL,
+      };
     } catch (error) {
       console.error("Error fetching data:", error);
       return { error: "Failed to fetch response." };
--- src/app-building/frontend/src/components/playground/PGDAlex.jsx
+++ src/app-building/frontend/src/components/playground/PGDAlex.jsx
@@ -1,11 +1,13 @@
 export const getPGDAlex = async (model, attack) => {
     const url = "http://34.138.29.34:8000/alexnet-attack/";
+    const imageUrl =
+    "http://34.138.29.34:8000/get-file/?file_path=figures/example_1_original_vs_adversarial.png";
     const payload = {
       model: model,
       attack: attack,
       epsilon: 0.2,
       eps_step: 0.01,
-      max_iter: 10,
+      max_iter: 1,
     };
   
     try {
@@ -22,10 +24,25 @@ export const getPGDAlex = async (model, attack) => {
       }
   
       const jsonResponse = await response.json();
-      return jsonResponse;
+
+      const imageResponse = await fetch(imageUrl);
+      if (!imageResponse.ok) {
+        throw new Error(`Failed to download image. Status: ${imageResponse.status}`);
+      }
+
+      const imageBlob = await imageResponse.blob();
+
+
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+
+      return {
+        json: jsonResponse,
+        image: imageObjectURL,
+      };
     } catch (error) {
       console.error("Error fetching data:", error);
       return { error: "Failed to fetch response." };
     }
   };
+  
   
