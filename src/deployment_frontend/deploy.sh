f425f51be6e4a0bf25e803a508c60d5af3e620d4
huckels15 <jacobhuckelbery@gmail.com>
2024-12-08 20:10:49 -0500

    fixed kubernetes

--- src/deployment_frontend/deploy.sh
+++ src/deployment_frontend/deploy.sh
@@ -1,4 +1,4 @@
-ansible-playbook deploy-docker-images.yml -i inventory.yml
-# ansible-playbook deploy-model-reg.yml -i inventory.yml
-ansible-playbook deploy-containers.yml -i inventory.yml
+# ansible-playbook deploy-docker-images.yml -i inventory.yml
+# # ansible-playbook deploy-model-reg.yml -i inventory.yml
+# ansible-playbook deploy-containers.yml -i inventory.yml
 ansible-playbook deploy-k8s-cluster.yml -i inventory.yml --extra-vars cluster_state=present
