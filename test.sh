ae577ada5d1662501c3e9be8fc0b5d9f55b8aa21
huckels15 <jacobhuckelbery@gmail.com>
2024-12-09 15:15:59 -0500

    removing erraneous files

--- test.sh
+++
@@ -1,7 +0,0 @@
-python train_alexnet.py \
-  --bucket_name="alexnet-data-multi" \
-  --data_path="data" \
-  --local_dir="./data" \
-  --epochs=100 \
-  --batch_size=32 \
-  --model_save_path="./models"
